608b15be93bb1f585e0419c3e276616c8c4dd874
ILYA <ilya.aurov@gmail.com>
2019-03-22 15:14:54 +0300

    fix> Переместил сбор статических файлов в докер контейнер

--- build/hasker_service/fire.sh
+++ build/hasker_service/fire.sh
@@ -4,5 +4,6 @@ python3 /opt/hasker/manage.py test
 if [[ $? -eq 0 ]]
 then
 
+    python3 /opt/hasker/manage.py collectstatic --noinput
     uwsgi --ini /opt/hasker/build/hasker_service/uwsgi.ini
 fi;
